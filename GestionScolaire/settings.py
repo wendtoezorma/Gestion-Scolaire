283abafe2e3a5edcc294146d83d13f03268f2439
Gedeonodg <154893753+Gedeonodg@users.noreply.github.com>
2024-07-24 16:50:09 +0000

    Résolution des conflits

--- GestionScolaire/settings.py
+++ GestionScolaire/settings.py
@@ -119,11 +119,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-<<<<<<< HEAD
 LANGUAGE_CODE = 'fr-FR'
-=======
-LANGUAGE_CODE = 'FR-FR'
->>>>>>> origin/master
 
 TIME_ZONE = 'UTC'
 
