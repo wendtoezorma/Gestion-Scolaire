a132cca46bf0b8f336f54c8efacfe30317e5a2a1
Gedeonodg <154893753+Gedeonodg@users.noreply.github.com>
2024-07-24 15:53:01 +0000

    Résolution des conflits

--- Administration/admin.py
+++ Administration/admin.py
@@ -9,14 +9,10 @@ from django.contrib.auth.admin import UserAdmin
     form = AdministrationAdminForm"""
     
 class AdminEtudiant(admin.ModelAdmin):
-<<<<<<< HEAD
     list_display=("matricule",'nom_etudiant','prenom_etudiant',"niveau_etudiant","filiere","email_etudiant","bourse","mdp_etudiant",'date_ajout')
 
 class AdminBoursier(admin.ModelAdmin):
     list_display=('type_bourse',"reduction")
-=======
-    list_display=("matricule",'nom_etudiant','prenom_etudiant',"email_etudiant","boursier","mdp_etudiant",'date_ajout')
->>>>>>> origin/master
 
 class AdminFiliere(admin.ModelAdmin):
     list_display=('nom_filiere',)
@@ -109,9 +105,5 @@ admin.site.register(Cours_Module,Adminmodule)
 admin.site.register(professeurs,AdminProfesseur)
 admin.site.register(Enseignement,AdminEnseignement)
 admin.site.register(Scolarite,AdminScolarite)
-<<<<<<< HEAD
 admin.site.register(Boursier,AdminBoursier)
-=======
-
->>>>>>> origin/master
 
