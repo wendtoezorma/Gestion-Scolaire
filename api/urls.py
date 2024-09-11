768ead72a4c9c732c73d151d46429317ac61eb9d
wendtoezorma <wendtoezorma@gmail.com>
2024-09-11 04:04:13 -0700

    retouche du cote api , cote fichier cours

--- api/urls.py
+++ api/urls.py
@@ -18,5 +18,8 @@ urlpatterns = [
     path('emploi_du_temps/', UploadedFileListView.as_view(), name='uploaded_file_list_api'),#pour lister les emploi du temps disponible
     path('emploi_du_temps/<int:file_id>/view/', display_table, name='display_table'),#pour voir un emploi du temps
     path('Douwnload_emploi_du_temps/<int:file_id>/download/', download_pdf, name='download_pdf'),#pour le telecharger
-    path('cours-fichiers/', CoursFichierAPI.as_view(), name='cours_fichiers_api'),#envoyer sous un format html
+    #path('cours-fichiers/', CoursFichierAPI.as_view(), name='cours_fichiers_api'),#envoyer sous un format html
+    path('cours-fichiers/', CoursFichierAPI.as_view(), name='cours_fichiers_api'),#envoyer sous un format json 
+    path('download-pdf/<int:file_id>/', download_pdf_cours, name='download_pdf'),
+    
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
