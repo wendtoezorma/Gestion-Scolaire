39ab6c171ae46358c58280437477f52a411c65d6
wendtoezorma <wendtoezorma@gmail.com>
2024-11-16 19:52:20 -0800

    Résolution des conflits après application du stash

--- Administration/paswords_generators.py
+++
@@ -1,8 +0,0 @@
-from random import sample,randint
-def generateur_mdp():    
-    password = "@##0AZERTYUI#OPQSD#F$GH@@@@$!@MWXCV!BNmlkjhgfdsqnbvpoiuytcxwreza@"
-    mdp=("".join(sample(password,8)))+str(randint(100,1000))
-    mdp_genere=""
-    for k in mdp.split():
-        mdp_genere+=k
-    return mdp_genere
--- Administration/templatetags/custom_filters.py
+++
@@ -1,8 +0,0 @@
-# myapp/templatetags/custom_tags.py
-from django import template
-
-register = template.Library()
-
-@register.filter
-def mul(value1, value2):
-    return value1 * value2
